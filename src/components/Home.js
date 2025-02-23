ee8bc5135e9f885f5076a9e9a77374c78cd78219
ryota <ryota200309@email.com>
2025-02-23 13:16:19 +0900

    レイアウトの調整

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,21 @@
 import React from "react";
+import "./Home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="homePage">
+      <div className="postContents">
+        <div className="postHeader">
+          <h1>タイトル</h1>
+        </div>
+        <div className="postTextContainer">今はReactの学習中です。</div>
+        <div className="nameAndDeleteButton">
+          <h3>@ryota</h3>
+          <button>削除</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
